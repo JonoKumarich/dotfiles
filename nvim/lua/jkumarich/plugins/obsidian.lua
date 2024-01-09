3262c159d815bc297d8f118f666670895b756e39
JonoKumarich <jonathankumarich@gmail.com>
2024-01-09 15:16:44 +0100

    surround plugin

--- nvim/lua/jkumarich/plugins/obsidian.lua
+++ nvim/lua/jkumarich/plugins/obsidian.lua
@@ -1,7 +1,7 @@
 return {
   "epwalsh/obsidian.nvim",
   version = "*",  -- recommended, use latest release instead of latest commit
-  lazy = true,
+  lazy = false,
   ft = "markdown",
   -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
   -- event = {
@@ -20,7 +20,7 @@ return {
     workspaces = {
       {
         name = "personal",
-        path = "/Users/jonathankumarich/Library/Mobile Documents/iCloud~md~obsidian/Document/My Vault",
+        path = "/Users/jonathankumarich/Library/Mobile Documents/iCloud~md~obsidian/Documents/My Vault",
       },
     },
     -- see below for full list of options 👇
