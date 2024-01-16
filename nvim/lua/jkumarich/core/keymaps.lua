590c8649d9c0217bd0f0dc97c4d8e562eff0c820
JonoKumarich <jonathankumarich@gmail.com>
2024-01-16 08:35:59 +0100

    Nvim config changes

--- nvim/lua/jkumarich/core/keymaps.lua
+++ nvim/lua/jkumarich/core/keymaps.lua
@@ -4,3 +4,13 @@ local keymap = vim.keymap
 keymap.set('v', '<', '<gv')
 keymap.set('v', '>', '>gv')
 
+-- Remove arrow keys. Home row only :)
+keymap.set('n', '<Up>', '<Nop>', { noremap = true, silent = true })
+keymap.set('n', '<Down>', '<Nop>', { noremap = true, silent = true })
+keymap.set('n', '<Left>', '<Nop>', { noremap = true, silent = true })
+keymap.set('n', '<Right>', '<Nop>', { noremap = true, silent = true })
+keymap.set('i', '<Up>', '<Nop>', { noremap = true, silent = true })
+keymap.set('i', '<Down>', '<Nop>', { noremap = true, silent = true })
+keymap.set('i', '<Left>', '<Nop>', { noremap = true, silent = true })
+keymap.set('i', '<Right>', '<Nop>', { noremap = true, silent = true })
+
