046fe3ce72f369032d57d6d8d003eeba6ed8763c
JonoKumarich <jonathankumarich@gmail.com>
2024-05-06 13:57:54 +0200

    Neorg setup

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -59,4 +59,12 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- Tabbing
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
+
 -- vim: ts=2 sts=2 sw=2 et
