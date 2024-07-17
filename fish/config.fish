3c6d3255648be1992ab2fde7ec76a295413393ba
JonoKumarich <jonathankumarich@gmail.com>
2024-07-17 15:17:34 +0200

    python venv improvements

--- fish/config.fish
+++ fish/config.fish
@@ -5,15 +5,12 @@ end
 set -gx PATH /opt/homebrew/bin $PATH
 set -gx PATH $PATH $HOME/.local/bin
 
-set -gx PIP_REQUIRE_VIRTUALENV true
-set -gx VIRTUAL_ENV .venv
 set -gx EDITOR nvim
+set -gx VIRTUAL_ENV ".venv/"
+
+set -Ua fish_user_paths "$HOME/.rye/shims"
 
 set -gx PATH $PATH $HOME/programs/kmonad/.stack-work/install/aarch64-osx/1bd4ca6412693104f43c209161dbc9884684a9af4ae1bb87b21e5ce70772abb4/9.4.8/bin/
-
-set -Ux PYENV_ROOT $HOME/.pyenv
-set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
-pyenv init - | source
 
 alias ls="eza --color=always --long --git --icons=always --no-time --no-user --no-permissions"
 alias nvim="nvimvenv"
--- nvim/init.lua
+++ nvim/init.lua
@@ -93,6 +93,9 @@ vim.g.maplocalleader = ","
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
+vim.g.python3_host_prog = "$HOME/.venv/bin/python3"
+vim.g.gruvbox_contrast_dark = "hard"
+
 -- [[ Setting options ]]
 require("options")
 
