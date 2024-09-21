bafe61e66168ab8ba4898d768cd1f51f4b8ff196
jonathankumarich <jonathankumarich@gmail.com>
2024-09-21 15:02:22 +0200

    Improvig config

--- fish/config.fish
+++ fish/config.fish
@@ -2,18 +2,17 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+alias brew="env PATH=(string replace (pyenv root)/shims '' \"\$PATH\") brew"
+
 set -gx PATH /opt/homebrew/bin $PATH
 set -gx PATH $PATH $HOME/.local/bin
 
 set -gx EDITOR nvim
 set -gx VIRTUAL_ENV ".venv/"
 
-set -Ua fish_user_paths "$HOME/.rye/shims"
-
 set -gx PATH $PATH $HOME/programs/kmonad/.stack-work/install/aarch64-osx/1bd4ca6412693104f43c209161dbc9884684a9af4ae1bb87b21e5ce70772abb4/9.4.8/bin/
 
 alias ls="eza --color=always --long --git --icons=always --no-time --no-user --no-permissions"
-alias nvim="nvimvenv"
 
 set -gx MCFLY_KEY_SCHEME vim
 mcfly init fish | source
@@ -21,4 +20,6 @@ mcfly init fish | source
 zoxide init fish --cmd cd | source
 
 fish_add_path $HOME/.spicetify
+
+pyenv init - | source
 
